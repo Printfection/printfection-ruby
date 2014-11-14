e3dac1648e85d67d344675041f71c957489a89ad
Casey O'Hara <casey.ohara@me.com>
2014-11-13 19:48:54 -0700

    Implement delete request

--- spec/printfection_spec.rb
+++ spec/printfection_spec.rb
@@ -157,3 +157,54 @@ describe Printfection, ".patch" do
   end
 end
 
+describe Printfection, ".delete" do
+  context "when it is successful" do
+    let(:raw_json)    { double(:raw_json) }
+    let(:response)    { double(:response, :body => raw_json) }
+    let(:parsed_json) { double(:parsed_json) }
+
+    before do
+      allow(RestClient).to receive(:delete).
+                           with("https://api.printfection.com/v2/path/to/resource/123").
+                           and_return(response)
+    end
+
+    context "when it can be parsed" do
+      before do
+        allow(JSON).to receive(:parse).
+                       with(raw_json).
+                       and_return(parsed_json)
+      end
+
+      it "returns the parsed JSON" do
+        json = Printfection.delete("/path/to/resource/123")
+        expect(json).to eql parsed_json
+      end
+    end
+
+    context "when it cannot be parsed" do
+      before do
+        allow(JSON).to receive(:parse).
+                       with(raw_json).
+                       and_raise(JSON::ParserError)
+      end
+
+      it "raises Error" do
+        expect {
+          Printfection.delete("/path/to/resource/123")
+        }.to raise_error Printfection::Error
+      end
+    end
+  end
+
+  context "when it is not successful" do
+    it "raises Error" do
+      allow(RestClient).to receive(:delete).
+                           and_raise(RestClient::ResourceNotFound)
+      expect {
+        Printfection.delete("/path/to/resource/123")
+      }.to raise_error Printfection::Error
+    end
+  end
+end
+
